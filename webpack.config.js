711574c3deeb9668525b41a4a3aed698441b84a8
Francisco do Vale <francisco_do_vale@epam.com>
2020-03-05 15:47:04 +0100

    chore(config): wip - css export

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/main.ts',
@@ -11,24 +12,45 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use:[
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+     },
+     {
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: '/dist',
+          },
+        },
+        'css-loader',
+      ],
+    }
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js' ],
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    extensions: ['.ts', '.js', 'scss'],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'BM - Movie Search',
       myPageHeader: 'Hello World',
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ],
   devServer: {
     contentBase: './dist',
     hot: true
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   }
 };
